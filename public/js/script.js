eadbc87e68a587e089ba83c50d6de6f64931233b
Aditya Singh Yadav <adityainprivate@gmail.com>
2024-10-24 23:42:04 +0530

    third

--- public/js/script.js
+++ public/js/script.js
@@ -11,3 +11,59 @@ tabs.forEach((tab) => {
     document.getElementById(formId).classList.add("active");
   });
 });
+
+document
+  .getElementById("create-rule-form")
+  .addEventListener("submit", async function (event) {
+    event.preventDefault();
+    const ruleName = document.getElementById("ruleName").value;
+    const ruleString = document.getElementById("ruleString").value;
+    const response = await fetch("/api/rules/create_rule", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ ruleName, ruleString }),
+    });
+    const result = await response.json();
+    console.log(result);
+  });
+
+// combine
+document
+  .getElementById("combine-rules-form")
+  .addEventListener("submit", async function (event) {
+    event.preventDefault();
+    const op = document.getElementById("operator1").value;
+    const rules = Array.from(
+      document.querySelectorAll('input[id^="combine-rule"]')
+    ).map((input) => input.value);
+    const response = await fetch("/api/rules/combine_rules", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ rules, op }),
+    });
+    const result = await response.json();
+    console.log(result);
+    
+  });
+
+// evaluate
+document
+  .getElementById("evaluate-rule-form")
+  .addEventListener("submit", async function (event) {
+    event.preventDefault();
+    const ast = document.getElementById("evaluate-ast").value;
+    const data = document.getElementById("evaluate-data").value;
+    const response = await fetch("/api/rules/evaluate_rule", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ ast, data: JSON.parse(data) }),
+    });
+    const result = await response.json();
+     console.log(result);
+  });
---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const path = require("path");
+const ruleRoutes = require("./routes/ruleRoutes");
+
+const app = express();
+const PORT = 3000;
+const CREATE_RULE = require("./routes/createRule");
+const COMBINE_RULE = require("./routes/combineRule");
+const EVALUATE_RULE = require("./routes/evaluateRule");
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(express.static("public"));
+
+// Serve static files from the public directory
+app.use(express.static(path.join(__dirname, "public")));
+
+// Route for serving the index.html
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./views/index.html"));
+});
+
+app.use("/create_rule", CREATE_RULE);
+app.use("/combine_rules", COMBINE_RULE);
+app.use("/evaluate_rule", EVALUATE_RULE);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
